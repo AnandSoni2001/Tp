d6f0f2284474c9cac25a9c434e41cabdcff3432d
Anand Soni <77532583+AnandSoni2001@users.noreply.github.com>
2023-06-04 14:34:23 +0530

    Update Homepage.py

--- Homepage.py
+++ Homepage.py
@@ -1,4 +1,30 @@
+import yaml
 import streamlit as st
+import streamlit_authenticator as stauth
+from yaml.loader import SafeLoader
+
+with open('/Users/asoni/Desktop/pass.yaml') as file:
+    config = yaml.load(file, Loader=SafeLoader)
+
+authenticator = stauth.Authenticate(
+    config['credentials'],
+    config['cookie']['name'],
+    config['cookie']['key'],
+    config['cookie']['expiry_days'],
+    config['preauthorized']
+)
+
+name, authentication_status, username = authenticator.login('Login', 'main')
+
+if authentication_status:
+    authenticator.logout('Logout', 'main', key='unique_key')
+    st.write(f'Welcome *{name}*')
+    st.title('Some content')
+elif authentication_status is False:
+    st.error('Username/password is incorrect')
+elif authentication_status is None:
+    st.warning('Please enter your username and password')
+
 
 st.set_page_config(
     page_title="Jewel",
