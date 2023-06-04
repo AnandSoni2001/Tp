72399da4de076fac9447dcc98fa1dd19082150cb
Anand Soni <77532583+AnandSoni2001@users.noreply.github.com>
2023-06-04 15:11:14 +0530

    Update 1_⬆︎_Input Data.py

--- pages/1_⬆︎_Input Data.py
+++ pages/1_⬆︎_Input Data.py
@@ -7,6 +7,8 @@ deta = Deta(st.secrets["key_number"])
 db = deta.Base("Jewel")
 db1 = deta.Base("Counter")
 
+st.write(db1.fetch())
+n=1
 st.set_page_config(page_title="Enter Data", page_icon="📈")
 
 today = datetime.date.today()
@@ -17,16 +19,16 @@ y=int(today.strftime("%Y"))
 with st.form("My"):
     col1, col2 = st.columns(2)
     with col1:
-        d = st.date_input("Issue Date", datetime.date(2023, 6, 3))
+        d = st.date_input("Issue Date", datetime.date(y, m, day))
         item = st.text_input('Item', '')
-        ghatpcs = st.number_input('Ghat PCs')
+        ghatpcs = st.number_input('Ghat PCs', step=1)
         pahad = st.number_input('Pahad Weight')
         kundan = st.number_input('Kundan Weight')
 
     with col2:
-        d1 = st.date_input("Receive Date", datetime.date(2023, 6, 3))
+        d1 = st.date_input("Receive Date", datetime.date(y, m, day))
         jadiyaname = st.text_input('Jadiya Name', '')
-        jobn = st.number_input('Job number', step=1)
+        jobn = st.number_input('Job number',value=n, step=1)
         gross = st.number_input('Gross Weight')
         chijat = st.number_input('Chijat')
 
@@ -36,6 +38,7 @@ with st.form("My"):
     submit_button = st.form_submit_button(label='Submit')
     
 if submit_button:
+    st.metric(label="Total", value=(kundan+chijat))
     db.put({"Issue Date" : date_time,
                 "Receive Date" : date_time_1,
                 "Item" : item,
