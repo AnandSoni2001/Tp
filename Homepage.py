75b2ba07990b0e42b5a0bca0d2f799c85fb3717d
Anand Soni <77532583+AnandSoni2001@users.noreply.github.com>
2023-06-09 21:59:26 +0530

    Update Homepage.py

--- Homepage.py
+++ Homepage.py
@@ -59,7 +59,7 @@ if authentication_status:
             if i['Job Number'] == number:
                 flag = 1
                 st.write(i)
-                df = pd.DataFrame.from_dict(i)
+                df = pd.DataFrame.from_dict(i, ,index=[0])
                 #st.write(df)
                 #df.to_csv("new.csv", index = False)
 
