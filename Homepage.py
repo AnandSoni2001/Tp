cf4d5a72de3ce76b3df983d71bee308187a93f2a
Anand Soni <77532583+AnandSoni2001@users.noreply.github.com>
2023-06-09 21:38:01 +0530

    Update Homepage.py

--- Homepage.py
+++ Homepage.py
@@ -61,20 +61,9 @@ if authentication_status:
                 st.write(i)
                 
         df = pd.DataFrame(i)
-        
-        def convert_df(df):
-            # IMPORTANT: Cache the conversion to prevent computation on every rerun
-            return df.to_csv().encode('utf-8')
+        st.write(df)
+        df.to_csv("new.csv", index = False)
 
-        csv = convert_df(my_large_df)
-
-        st.download_button(
-            label="Download data as CSV",
-            data=csv,
-            file_name='large_df.csv',
-            mime='text/csv',
-        )
-                
         if flag == 0:
             st.write('Data not found')
 
