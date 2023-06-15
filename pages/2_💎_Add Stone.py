b3bcab32e16aaac69862d3e7e7f199da9b8b99de
Anand Soni <77532583+AnandSoni2001@users.noreply.github.com>
2023-06-15 16:43:27 +0530

    Update 2_💎_Add Stone.py

--- pages/2_💎_Add Stone.py
+++ pages/2_💎_Add Stone.py
@@ -12,27 +12,27 @@ with st.form("My"):
   submit_button = st.form_submit_button(label='Submit')
     
 if submit_button:
-  db.put({"Stone name" : stone})
-  st.write('Stone has been added to list') 
+  flag = 0
+  
+  for i in all_items:
+    if upper(i['Stone name']) == upper(stone):
+        st.write('Stone already present')
+        flag = 1
+        break
+        
+  if flag == 0:
+    db.put({"Stone name" : stone})
+    st.write('Stone has been added to list') 
   
 a = st.button('View Stone')  
 if a:
   res = db.fetch()
   all_items = res.items
   try:
-      df = pd.DataFrame(all_items)
-
-      df_print = df.drop('key', axis=0)
+    for i in all_items:
+      df = pd.DataFrame(i, index=[1])
+      df_print = df.drop('key', axis=1)
       st.write(df_print)
-
-      csv = convert_df(df_print)
-
-      st.download_button(
-          label="Download all data",
-          data=csv,
-          file_name='data.csv',
-          mime='text/csv',
-      )
       
-   except:
-      st.write('No data to display')
+  except:
+    st.write('No data to display')
