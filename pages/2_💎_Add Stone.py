c3ee2872f875ba20ef4772b0272d50c8cc060788
Anand Soni <77532583+AnandSoni2001@users.noreply.github.com>
2023-06-15 16:47:00 +0530

    Update 2_💎_Add Stone.py

--- pages/2_💎_Add Stone.py
+++ pages/2_💎_Add Stone.py
@@ -9,19 +9,16 @@ db = deta.Base("Stone")
 res = db.fetch()
 all_items = res.items
 
-with st.form("My"):
-  stone = st.text_input('Add new stone', 'Enter name')
+with st.form("New"):
+  stone = st.text_input('Add new stone', '')
   submit_button = st.form_submit_button(label='Submit')
     
 if submit_button:
-  flag = 0
-  
+  flag = 0  
   for i in all_items:
     if upper(i['Stone name']) == upper(stone):
         st.write('Stone already present')
-        flag = 1
-        break
-        
+        flag = 1        
   if flag == 0:
     db.put({"Stone name" : stone})
     st.write('Stone has been added to list') 
