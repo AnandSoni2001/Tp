1715913b0e5578319fe423b59ddc15d2b37a8f81
Anand Soni <77532583+AnandSoni2001@users.noreply.github.com>
2023-06-17 12:42:40 +0530

    Update 3_⬇︎_Recieved Item.py

--- pages/3_⬇︎_Recieved Item.py
+++ pages/3_⬇︎_Recieved Item.py
@@ -100,3 +100,9 @@ if genre == 'Partial' :
                              keydata = str(j['key'])
                              db.update({"Receive Date": d11, "Comments" : cmts1, "Receive Amount" : amt1, "Stones": stone, "PCs":pc},keydata,)
                              st.write("Data submitted !")
+               
+                 if str(j['Status']) == 'F':
+                        st.write("This items is already fully receieved !")
+                        
+                 if str(j['Status']) == 'P':
+                        st.write("This items is already partially receieved !")
